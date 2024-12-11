dbd8358692e60f09c2442aa3df5c5fa0c3726bde
Arron Chapman <unkwntech@gmail.com>
2024-12-11 17:15:58 -0600

    Fixed add char button

--- src/objects/users.object.tsx
+++ src/objects/users.object.tsx
@@ -19,23 +19,20 @@ import {
     useRecordContext,
 } from "react-admin";
 
-//<ReferenceArrayField label="# Tokens" reference='tokens' source='tokens'></ReferenceArrayField>
-const redirectCCPOauth = (a: any) => {
-    window.localStorage.setItem("ActiveAdd", "true");
-    //https://login.eveonline.com/v2/oauth/authorize?response_type=code&client_id=91407ce2465e47a2b0d3844fa801f15c&redirect_uri=http://localhost:5173/callback/&scope=esi-search.search_structures.v1,esi-universe.read_structures.v1,esi-corporations.read_structures.v1,esi-characters.read_notifications.v1&state=asdf123
+const redirectOauth = () =>
     window.location.assign(
-        "https://login.eveonline.com/v2/oauth/authorize?response_type=code&client_id=91407ce2465e47a2b0d3844fa801f15c&redirect_uri=http://localhost:5173/%23/auth-callback/&scope=esi-search.search_structures.v1 esi-universe.read_structures.v1 esi-corporations.read_structures.v1 esi-characters.read_notifications.v1&state=asdf123"
+        "https://login.eveonline.com/v2/oauth/authorize?response_type=code&client_id=91407ce2465e47a2b0d3844fa801f15c&redirect_uri=http://notifs.ibns.tech/%23/auth-callback/&scope=esi-search.search_structures.v1 esi-universe.read_structures.v1 esi-corporations.read_structures.v1 esi-characters.read_notifications.v1&state=asdf123".replace(
+            " ",
+            "%20"
+        )
     );
-};
-
-const thingo = () => alert("thingo");
 
 const CreateMenuItemButton = () => (
     <Button
         resource="character"
         label="Add"
         children={<PersonAddAltOutlinedIcon />}
-        onClick={thingo}
+        onClick={redirectOauth}
     />
 );
 
